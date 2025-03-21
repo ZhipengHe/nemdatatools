0264e939ae0efb6698e92c3b8d6a79692438422d
Zhipeng He <me.zhipeng.he@gmail.com>
2025-03-21 23:17:20 +1000

    feat(init): enhance module imports and define public API surface

--- src/nemdatatools/__init__.py
+++ src/nemdatatools/__init__.py
@@ -1,3 +1,21 @@
 """NEMDataTools: Tools for accessing and preprocessing AEMO data."""
 
 __version__ = "0.1.0"
+
+from nemdatatools.data_source import NEM_REGIONS, DataSource
+
+# Import key modules for easy access
+from nemdatatools.downloader import (
+    check_connection,
+    fetch_data,
+    get_available_data_types,
+)
+
+# Define what's accessible via import *
+__all__ = [
+    "NEM_REGIONS",
+    "DataSource",
+    "check_connection",
+    "fetch_data",
+    "get_available_data_types",
+]
